e549e838e51907cfe26884b9eba5723e987e34e1
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2025-11-03 20:02:36 +0530

    complete the howtiworks

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Cta } from "@/components/Landing/Cta";
 import { Footer } from "@/components/Landing/Footer";
 import  Header  from "@/components/Landing/Header";
 import  Hero  from "@/components/Landing/Hero";
-import { HowitWorks } from "@/components/Landing/HowitWorks";
+import  HowitWorks  from "@/components/Landing/HowitWorks";
 import { PricingSection } from "@/components/Landing/PricingSection";
 import { WhattoAsk } from "@/components/Landing/WhattoAsk";
 

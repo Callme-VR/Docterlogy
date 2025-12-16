66e2040a0a315e1c28de82e491f4dcbe7e07f2e9
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2025-12-16 23:33:45 +0530

    add

--- components/Dashboard/WelcomeSection.tsx
+++ components/Dashboard/WelcomeSection.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function WelcomeSection() {
   const { user } = useUser();
   return (
-    <div className="relative z-10 flex items-center justify-between bg-gradient-to-br from-primary/10 via-primary/5 to-background rounded-3xl p-8 border border-primary/20 mb-12 overflow-hidden">
+    <div className="relative z-10 flex items-center justify-between bg-linear-to-br from-primary/10 via-primary/5 to-background rounded-3xl p-8 border border-primary/20 mb-12 overflow-hidden">
       {/* for welcome section box upper one */}
       <div className="space-y-4">
         <div className="inline-flex items-center gap-3 px-3 py-1 bg-primary/20 rounded-full border border-primary/30">
@@ -34,7 +34,7 @@ export default function WelcomeSection() {
       </div>
 
       {/* for the Badge of icon */}
-      <div className="lg:flex hidden items-center w-20 h-20 bg-gradient-to-br from-primary/30 to-primary/40 rounded-full">
+      <div className="lg:flex hidden items-center w-20 h-20 bg-linear-to-br from-primary/30 to-primary/40 rounded-full">
           <Image src="/logo.png" alt="doctor" width={60} height={60} />
         
       </div>

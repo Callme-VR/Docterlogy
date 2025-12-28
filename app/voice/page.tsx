2b6205ec14f36f4cbb186493c63137588c464563
Callme-VR <176268004+Callme-VR@users.noreply.github.com>
2025-12-28 16:02:44 +0530

    add

--- app/voice/page.tsx
+++ app/voice/page.tsx
@@ -1,27 +1,29 @@
 import Navbar from "@/components/Navbar";
-import  FeatureCard  from "@/components/voice/FeatureCard";
+import FeatureCard from "@/components/voice/FeatureCard";
 import ProPlanRequired from "@/components/voice/ProPlanRequired";
 import VapiWidget from "@/components/voice/VapiWidget";
 import WelcomeSection from "@/components/voice/WelcomeSection";
 import { auth } from "@clerk/nextjs/server";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Voice | DocterLogy",
+  description: " it is an Voice Assistant for doctors ",
+};
 
 export default async function Voice() {
+  const { has } = await auth();
+  const hasProPlan = has({ plan: "ai_basic" }) || has({ plan: "ai_pro" });
+  if (!hasProPlan) return <ProPlanRequired />;
 
-    const { has } = await auth();
-    const hasProPlan = has({ plan: "ai_basic" }) || has({ plan: "ai_pro" });
-    if (!hasProPlan) return <ProPlanRequired />;
-
-
-    return (
-        <div className="min-h-screen bg-background">
-            <Navbar />
-            <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
-                <WelcomeSection />
-                <FeatureCard />
-
-            </div>
-            <VapiWidget />
-        </div>
-    )
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <FeatureCard />
+      </div>
+      <VapiWidget />
+    </div>
+  );
 }
